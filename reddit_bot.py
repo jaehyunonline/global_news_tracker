2dbcf431e2bd5a5ba7c5b853230345bf3ab0b82e
yk <rlduf1028@gmail.com>
2024-08-30 11:19:34 +0900

    f

--- reddit_bot.py
+++ reddit_bot.py
@@ -8,21 +8,38 @@ reddit = praw.Reddit(client_id='ca9Fu2aX5UjAuYXvYSLcjQ',
 
 
 
-RESULT_MAX_CNT = 10
+MAX_RESULT_CNT = 10
+MAX_BODY_CHAR_CNT = 250
+
 
 
 
 result = {}
 
 
+def extract_text(text, keyword, context_length = (MAX_BODY_CHAR_CNT/2)):
+    # 키워드 위치 찾기
+    keyword_position = text.find(keyword)
+    
+    if keyword_position == -1:
+        return "Keyword not found in the text."
+
+    # 앞뒤로 context_length만큼 잘라내기
+    start = max(keyword_position - context_length, 0)
+    end = min(keyword_position + len(keyword) + context_length, len(text))
+
+    # 잘라낸 텍스트 반환
+    return text[start:end]
+
 
 def get_result(keyword):
     # 'all' 서브레딧에서 특정 키워드로 가장 최근 게시물 검색
     subreddit = reddit.subreddit('all')
-    recent_posts = subreddit.search(keyword, sort='new', limit=RESULT_MAX_CNT)
+    recent_posts = subreddit.search(keyword, sort='new', limit=MAX_RESULT_CNT)
 
     title = []
-    aaa = []
+    body = []
+    source = []
     issued_time = []
     url = []
 
@@ -37,14 +54,17 @@ def get_result(keyword):
         # 게시물 내용 (selftext) 가져오기
         post_content = post.selftext if post.selftext else "No content available"
 
+
+
         title.append(post.title)
-        aaa.append('reddit')
+        body.append(extract_text(post_content, keyword))
+        source.append('reddit')
         issued_time.append(formatted_time)
         url.append(post.url)
 
         #result = {'제목': [post.title], '언론사': ['reddit'], '발행시간': [formatted_time], '링크': [post.url]}
     
-    result = {'제목': title, '언론사': aaa, '발행시간': issued_time, '링크': url}
+    result = {'제목': title, '본문' : body, '언론사': source, '발행시간': issued_time, '링크': url}
 
     print(result)
 
