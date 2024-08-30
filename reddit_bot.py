4fa0d6a8cd86a327a2b0799349a58a0774c0cfda
yk <rlduf1028@gmail.com>
2024-08-30 15:57:28 +0900

    f

--- reddit_bot.py
+++ reddit_bot.py
@@ -15,6 +15,8 @@ MAX_BODY_CHAR_CNT = 120
 
 
 result = {}
+current_time = datetime.utcnow()
+one_month_ago = current_time - timedelta(days=30)
 
 
 def extract_text(text, keyword, context_length = MAX_BODY_CHAR_CNT):
@@ -35,7 +37,20 @@ def extract_text(text, keyword, context_length = MAX_BODY_CHAR_CNT):
 def get_result(keyword):
     # 'all' 서브레딧에서 특정 키워드로 가장 최근 게시물 검색
     subreddit = reddit.subreddit('all')
-    recent_posts = subreddit.search(keyword, sort='new', limit=MAX_RESULT_CNT)
+    recent_posts = subreddit.search(
+            keyword, 
+            sort='relevance', 
+            time_filter='month', 
+            limit=30
+    )
+
+    filtered_posts = [
+    post for post in recent_posts 
+    if datetime.utcfromtimestamp(post.created_utc) >= one_month_ago
+    ]
+
+    filtered_posts.sort(key=lambda x: x.created_utc, reverse=True)
+
 
     title = []
     body = []
@@ -43,7 +58,12 @@ def get_result(keyword):
     issued_time = []
     url = []
 
-    for post in recent_posts:
+    print('\n')
+
+    idx = 0
+
+    for post in filtered_posts:
+        idx += 1
         # Unix 타임스탬프를 UTC datetime 객체로 변환
         created_time_utc = datetime.utcfromtimestamp(post.created_utc)
         # KST (UTC+9) 시간대로 변환
@@ -52,9 +72,9 @@ def get_result(keyword):
         formatted_time = created_time_kst.strftime('%Y-%m-%d %H:%M:%S KST')
         
         # 게시물 내용 (selftext) 가져오기
-        post_content = post.selftext if post.selftext else "No content available"
+        post_content = post.selftext if post.selftext else "본문 없음"
 
-        print(f'{post_content}\n')
+        #print(f'{post_content}\n')
 
 
 
@@ -64,7 +84,14 @@ def get_result(keyword):
         issued_time.append(formatted_time)
         url.append(post.url)
 
+        print (f'{post.title} {formatted_time} {post.url}')
+        # print (f'{extract_text(post_content, keyword)}\n')
+        print (f'{post_content}\n')
+
         #result = {'제목': [post.title], '언론사': ['reddit'], '발행시간': [formatted_time], '링크': [post.url]}
+
+        if idx == MAX_RESULT_CNT:
+            break
     
     result = {'제목': title, '본문' : body, '언론사': source, '발행시간': issued_time, '링크': url}
 
@@ -75,5 +102,5 @@ def get_result(keyword):
 
 
 # result = {'제목': 'test tweets', '언론사': 'twitter', '발행시간': '2024-08-29 14:45 +09:00', '링크': 'https://x.com/login'}
-# get_result('Youtube')
+get_result('Youtube')
 
